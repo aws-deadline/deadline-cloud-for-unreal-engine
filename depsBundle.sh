c07eff7ade5f8e73cfdc2a43d85e7bf9f0df5258
Morgan Epp <60796713+epmog@users.noreply.github.com>
2024-02-21 18:52:34 -0600

    fix: add executable bit to depsBundle (#9)
    Signed-off-by: Morgan Epp <60796713+epmog@users.noreply.github.com>

--- depsBundle.sh
+++ depsBundle.sh
@@ -3,6 +3,6 @@ set -xeuo pipefail
 
 python depsBundle.py
 
-rm -f dependency_bundle/deadline_cloud_for_unreal_submitter-deps-windows.zip
+rm -f dependency_bundle/deadline_cloud_for_unreal_engine_submitter-deps-windows.zip
 
-cp dependency_bundle/deadline_cloud_for_unreal_submitter-deps.zip dependency_bundle/deadline_cloud_for_unreal_submitter-deps-windows.zip
+cp dependency_bundle/deadline_cloud_for_unreal_engine_submitter-deps.zip dependency_bundle/deadline_cloud_for_unreal_engine_submitter-deps-windows.zip
