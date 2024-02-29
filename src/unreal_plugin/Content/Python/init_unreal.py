ac7af200f398f37c44b817b3919243602e8cb61d
Eric Osiowy <134728121+erico-aws@users.noreply.github.com>
2024-02-29 03:49:42 +0000

    fix: Add imports to init_unreal to fully load the plugin.
    Signed-off-by: Eric Osiowy <134728121+erico-aws@users.noreply.github.com>

--- src/unreal_plugin/Content/Python/init_unreal.py
+++ src/unreal_plugin/Content/Python/init_unreal.py
@@ -15,3 +15,10 @@ if remote_execution != "True":
     unreal.log(f'DEADLINE CLOUD PATH: {os.getenv("DEADLINE_CLOUD")}')
     if os.getenv("DEADLINE_CLOUD") and os.environ["DEADLINE_CLOUD"] not in sys.path:
         sys.path.append(os.environ["DEADLINE_CLOUD"])
+
+    # These unused imports are REQUIRED!!!
+    # Unreal Engine loads any init_unreal.py it finds in its search paths.
+    # These imports finish the setup for the plugin.
+    from settings import DeadlineCloudDeveloperSettingsImplementation  # noqa: F401
+    from job_library import DeadlineCloudJobBundleLibraryImplementation  # noqa: F401
+    import remote_executor  # noqa: F401
