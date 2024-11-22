4d8686369bc05a04d63720379f158d227f4b4082
Ilona Alekseeva <i.alekseeva@icvr.io>
2024-11-22 10:34:02 +0300

    manually add enum values

--- src/unreal_plugin/Source/UnrealDeadlineCloudService/Public/PythonAPILibraries/PythonYamlLibrary.h
+++ src/unreal_plugin/Source/UnrealDeadlineCloudService/Public/PythonAPILibraries/PythonYamlLibrary.h
@@ -19,9 +19,9 @@ UENUM(BlueprintType)
 enum class EValueType : uint8
 {
 	INT = 0 UMETA(DisplayName = "Integer"),
-	FLOAT  UMETA(DisplayName = "Float"),
-	STRING  UMETA(DisplayName = "String"),
-	PATH    UMETA(DisplayName = "Path")
+	FLOAT = 1 UMETA(DisplayName = "Float"),
+	STRING = 2 UMETA(DisplayName = "String"),
+	PATH  = 3  UMETA(DisplayName = "Path")
 };
 
 USTRUCT(BlueprintType)
