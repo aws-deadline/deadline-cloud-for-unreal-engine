f5c34788dda9e4c13757de012088d08287edbf33
a.frolov <a.frolov@icvr.io>
2024-11-25 15:39:05 +0500

    ugs job submitter

---
+++ scripts/submission/ugs_render_job_submission.py
@@ -0,0 +1,58 @@
+import os
+import unreal
+from deadline.unreal_submitter.submitter import (
+    UnrealRenderOpenJobSubmitter,
+)
+
+from deadline.unreal_submitter.unreal_open_job.unreal_open_job import (
+    UgsRenderUnrealJob,
+)
+
+from deadline.unreal_submitter.unreal_open_job.unreal_open_job_step import (
+    UgsRenderUnrealOpenJobStep,
+    UnrealOpenJobStepParameterDefinition
+)
+
+from deadline.unreal_submitter.unreal_open_job.unreal_open_job_environment import (
+    UgsLaunchEditorUnrealOpenJobEnvironment,
+    UgsSyncCmfUnrealOpenJobEnvironment
+)
+from deadline.unreal_logger import get_logger
+
+
+logger = get_logger()
+
+
+if "OPENJD_TEMPLATES_DIRECTORY" not in os.environ:
+    os.environ["OPENJD_TEMPLATES_DIRECTORY"] = (
+        f"{os.path.dirname(os.path.dirname(__file__))}"
+        f"/src/unreal_plugin/Content/Python/openjd_templates"
+    )
+
+
+def main():
+    queue = unreal.get_editor_subsystem(unreal.MoviePipelineQueueSubsystem).get_queue()
+    render_job_submitter = UnrealRenderOpenJobSubmitter(silent_mode=True)
+
+    for job in queue.get_jobs():
+        default_render_job = UgsRenderUnrealJob(
+            steps=[
+                UgsRenderUnrealOpenJobStep(
+                    extra_parameters=[UnrealOpenJobStepParameterDefinition("ChunkSize", "INT", [10])]
+                )
+            ],
+            environments=[
+                UgsSyncCmfUnrealOpenJobEnvironment(), UgsLaunchEditorUnrealOpenJobEnvironment()
+            ],
+            mrq_job=job
+        )
+
+        render_job_submitter.add_job(default_render_job)
+
+    submitted_job_ids = render_job_submitter.submit_jobs()
+    for job_id in submitted_job_ids:
+        logger.info(f"Job submitted: {job_id}")
+
+
+if __name__ == '__main__':
+    main()
