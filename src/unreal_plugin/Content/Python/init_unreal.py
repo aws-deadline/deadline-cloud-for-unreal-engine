f6a07e7a0aa5907d7a90c9a993afa8d8b293328d
Brian Axelson <86568017+baxeaz@users.noreply.github.com>
2024-12-04 02:44:17 +0000

    fix: Importing logger after adding install libraries to sys.path.  Setting uninstall files in submitter installer.  Adding logger to submitter installer.
    Signed-off-by: Brian Axelson <86568017+baxeaz@users.noreply.github.com>

--- src/unreal_plugin/Content/Python/init_unreal.py
+++ src/unreal_plugin/Content/Python/init_unreal.py
@@ -5,20 +5,20 @@ import sys
 
 remote_execution = os.getenv("REMOTE_EXECUTION", "False")
 if remote_execution != "True":
+    libraries_path = f"{os.path.dirname(__file__)}/libraries".replace("\\", "/")
+    if not os.getenv("DEADLINE_CLOUD") and os.path.exists(libraries_path):
+        os.environ["DEADLINE_CLOUD"] = libraries_path
+
+    if os.getenv("DEADLINE_CLOUD") and os.environ["DEADLINE_CLOUD"] not in sys.path:
+        sys.path.append(os.environ["DEADLINE_CLOUD"])
+
     from deadline.unreal_logger import get_logger
 
     logger = get_logger()
 
     logger.info("INIT DEADLINE CLOUD")
 
-    libraries_path = f"{os.path.dirname(__file__)}/libraries".replace("\\", "/")
-    if not os.getenv("DEADLINE_CLOUD") and os.path.exists(libraries_path):
-        os.environ["DEADLINE_CLOUD"] = libraries_path
-
     logger.info(f'DEADLINE CLOUD PATH: {os.getenv("DEADLINE_CLOUD")}')
-    if os.getenv("DEADLINE_CLOUD") and os.environ["DEADLINE_CLOUD"] not in sys.path:
-        sys.path.append(os.environ["DEADLINE_CLOUD"])
-
     # These unused imports are REQUIRED!!!
     # Unreal Engine loads any init_unreal.py it finds in its search paths.
     # These imports finish the setup for the plugin.
