c0bc6a40e4ece822cd51254832f5c8cb260fce26
a.frolov <a.frolov@icvr.io>
2025-01-14 23:41:29 +0500

    restore missed Content/Python/libraries in os.environ and sys.path

--- src/unreal_plugin/Content/Python/init_unreal.py
+++ src/unreal_plugin/Content/Python/init_unreal.py
@@ -22,11 +22,20 @@ if remote_execution != "True":
     if actions_path not in sys.path:
         sys.path.append(actions_path)
 
+    libraries_path = f"{os.path.dirname(__file__)}/libraries".replace("\\", "/")
+    if not os.getenv("DEADLINE_CLOUD") and os.path.exists(libraries_path):
+        os.environ["DEADLINE_CLOUD"] = libraries_path
+
+    if os.getenv("DEADLINE_CLOUD") and os.environ["DEADLINE_CLOUD"] not in sys.path:
+        sys.path.append(os.environ["DEADLINE_CLOUD"])
+
     from deadline.unreal_logger import get_logger
 
     logger = get_logger()
 
     logger.info("INIT DEADLINE CLOUD")
+
+    logger.info(f'DEADLINE CLOUD PATH: {os.getenv("DEADLINE_CLOUD")}')
 
     # These unused imports are REQUIRED!!!
     # Unreal Engine loads any init_unreal.py it finds in its search paths.
