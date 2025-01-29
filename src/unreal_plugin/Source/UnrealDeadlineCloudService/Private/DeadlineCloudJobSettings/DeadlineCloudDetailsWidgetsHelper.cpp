442932f353841f7c198282654c3837d90aaa1512
Ilona Alekseeva <i.alekseeva@icvr.io>
2025-01-29 18:52:21 +0300

    add SetValue error log

--- src/unreal_plugin/Source/UnrealDeadlineCloudService/Private/DeadlineCloudJobSettings/DeadlineCloudDetailsWidgetsHelper.cpp
+++ src/unreal_plugin/Source/UnrealDeadlineCloudService/Private/DeadlineCloudJobSettings/DeadlineCloudDetailsWidgetsHelper.cpp
@@ -11,222 +11,226 @@
 class  SDeadlineCloudFilePathWidget : public SCompoundWidget
 {
 public:
-    SLATE_BEGIN_ARGS(SDeadlineCloudFilePathWidget) {}
-        SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PathPropertyHandle)
-    SLATE_END_ARGS()
-    void Construct(const FArguments& InArgs);
+	SLATE_BEGIN_ARGS(SDeadlineCloudFilePathWidget) {}
+		SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PathPropertyHandle)
+	SLATE_END_ARGS()
+	void Construct(const FArguments& InArgs);
 private:
-    TSharedPtr<IPropertyHandle> PathProperty;
-    FString GetSelectedFilePath() const;
-    void OnPathPicked(const FString& PickedPath);
+	TSharedPtr<IPropertyHandle> PathProperty;
+	FString GetSelectedFilePath() const;
+	void OnPathPicked(const FString& PickedPath);
 };
 
 void SDeadlineCloudFilePathWidget::Construct(const FArguments& InArgs)
 {
-    PathProperty = InArgs._PathPropertyHandle;
-    ChildSlot
-        [
-            SNew(SFilePathPicker)
-                .BrowseButtonImage(FAppStyle::GetBrush("PropertyWindow.Button_Ellipsis"))
-                .BrowseButtonStyle(FAppStyle::Get(), "HoverHintOnly")
-                .BrowseButtonToolTip(LOCTEXT("FileButtonToolTipText", "Choose a file from this computer"))
-                .BrowseDirectory(FEditorDirectories::Get().GetLastDirectory(ELastDirectory::GENERIC_OPEN))
-                .BrowseTitle(LOCTEXT("PropertyEditorTitle", "File picker..."))
-                .FilePath(this, &SDeadlineCloudFilePathWidget::GetSelectedFilePath)
-                .OnPathPicked(this, &SDeadlineCloudFilePathWidget::OnPathPicked)
-        ];
+	PathProperty = InArgs._PathPropertyHandle;
+	ChildSlot
+		[
+			SNew(SFilePathPicker)
+				.BrowseButtonImage(FAppStyle::GetBrush("PropertyWindow.Button_Ellipsis"))
+				.BrowseButtonStyle(FAppStyle::Get(), "HoverHintOnly")
+				.BrowseButtonToolTip(LOCTEXT("FileButtonToolTipText", "Choose a file from this computer"))
+				.BrowseDirectory(FEditorDirectories::Get().GetLastDirectory(ELastDirectory::GENERIC_OPEN))
+				.BrowseTitle(LOCTEXT("PropertyEditorTitle", "File picker..."))
+				.FilePath(this, &SDeadlineCloudFilePathWidget::GetSelectedFilePath)
+				.OnPathPicked(this, &SDeadlineCloudFilePathWidget::OnPathPicked)
+		];
 }
 
 void SDeadlineCloudFilePathWidget::OnPathPicked(const FString& PickedPath)
 {
-    PathProperty->SetValue(PickedPath);
+	FPropertyAccess::Result PathResult = PathProperty->SetValue(PickedPath);
+	if (PathResult != FPropertyAccess::Success)
+	{
+		UE_LOG(LogTemp, Error, TEXT("SetValue failed! Result: %d"), static_cast<int32>(PathResult));
+	}
 }
 
 FString SDeadlineCloudFilePathWidget::GetSelectedFilePath() const
 {
-    FString FilePath;
-    PathProperty->GetValue(FilePath);
+	FString FilePath;
+	PathProperty->GetValue(FilePath);
 
-    return FilePath;
+	return FilePath;
 }
 
 class SDeadlineCloudStringWidget : public SCompoundWidget
 {
 public:
-    SLATE_BEGIN_ARGS(SDeadlineCloudStringWidget) {}
-        SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, StringPropertyHandle)
-    SLATE_END_ARGS()
+	SLATE_BEGIN_ARGS(SDeadlineCloudStringWidget) {}
+		SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, StringPropertyHandle)
+	SLATE_END_ARGS()
 
-    void Construct(const FArguments& InArgs)
-    {
-        StringProperty = InArgs._StringPropertyHandle;
+	void Construct(const FArguments& InArgs)
+	{
+		StringProperty = InArgs._StringPropertyHandle;
 
-        ChildSlot
-            [
-                SNew(SHorizontalBox)
-                    + SHorizontalBox::Slot()
-                    .FillWidth(1.0f)
-                    .VAlign(VAlign_Center)
-                    [
-                        SNew(SEditableTextBox)
-                            .Font(IDetailLayoutBuilder::GetDetailFont())
-                            .OnTextChanged(this, &SDeadlineCloudStringWidget::HandleTextChanged)
-                            .Text(this, &SDeadlineCloudStringWidget::GetText)
-                    ]
-            ];
-    }
+		ChildSlot
+			[
+				SNew(SHorizontalBox)
+					+ SHorizontalBox::Slot()
+					.FillWidth(1.0f)
+					.VAlign(VAlign_Center)
+					[
+						SNew(SEditableTextBox)
+							.Font(IDetailLayoutBuilder::GetDetailFont())
+							.OnTextChanged(this, &SDeadlineCloudStringWidget::HandleTextChanged)
+							.Text(this, &SDeadlineCloudStringWidget::GetText)
+					]
+			];
+	}
 
 private:
-    void HandleTextChanged(const FText& NewText)
-    {
-        StringProperty->SetValue(NewText.ToString());
-    }
+	void HandleTextChanged(const FText& NewText)
+	{
+		StringProperty->SetValue(NewText.ToString());
+	}
 
-    FText GetText() const
-    {
-        FString String;
-        StringProperty->GetValue(String);
+	FText GetText() const
+	{
+		FString String;
+		StringProperty->GetValue(String);
 
-        return FText::FromString(String);
-    }
+		return FText::FromString(String);
+	}
 
-    TSharedPtr<IPropertyHandle> StringProperty;
+	TSharedPtr<IPropertyHandle> StringProperty;
 };
 
 class SDeadlineCloudIntWidget : public SCompoundWidget
 {
 public:
-    SLATE_BEGIN_ARGS(SDeadlineCloudIntWidget) {}
-        SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PropertyHandle)
-    SLATE_END_ARGS()
+	SLATE_BEGIN_ARGS(SDeadlineCloudIntWidget) {}
+		SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PropertyHandle)
+	SLATE_END_ARGS()
 
-    void Construct(const FArguments& InArgs)
-    {
-        Property = InArgs._PropertyHandle;
+	void Construct(const FArguments& InArgs)
+	{
+		Property = InArgs._PropertyHandle;
 
-        ChildSlot
-            [
-                SNew(SHorizontalBox)
-                    + SHorizontalBox::Slot()
-                    .FillWidth(1.0f)
-                    .VAlign(VAlign_Center)
-                    [
-                        SNew(SNumericEntryBox<int32>)
-                            .Font(IDetailLayoutBuilder::GetDetailFont())
-                            .AllowSpin(false)
-                            .MinDesiredValueWidth(50.0f)
-                            .Value_Lambda([this]
-                                {
-                                    FString String;
-                                    Property->GetValue(String);
-                                    return FCString::Atoi(*String);
-                                })
-                            .OnValueCommitted_Lambda([this](int32 Value, ETextCommit::Type)
-                                {
-                                    Property->SetValue(FString::FromInt(Value));
-                                })
-                    ]
-            ];
-    }
+		ChildSlot
+			[
+				SNew(SHorizontalBox)
+					+ SHorizontalBox::Slot()
+					.FillWidth(1.0f)
+					.VAlign(VAlign_Center)
+					[
+						SNew(SNumericEntryBox<int32>)
+							.Font(IDetailLayoutBuilder::GetDetailFont())
+							.AllowSpin(false)
+							.MinDesiredValueWidth(50.0f)
+							.Value_Lambda([this]
+								{
+									FString String;
+									Property->GetValue(String);
+									return FCString::Atoi(*String);
+								})
+							.OnValueCommitted_Lambda([this](int32 Value, ETextCommit::Type)
+								{
+									Property->SetValue(FString::FromInt(Value));
+								})
+					]
+			];
+	}
 
 private:
 
-    TSharedPtr<IPropertyHandle> Property;
+	TSharedPtr<IPropertyHandle> Property;
 };
 
 class SDeadlineCloudFloatWidget : public SCompoundWidget
 {
 public:
-    SLATE_BEGIN_ARGS(SDeadlineCloudFloatWidget) {}
-        SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PropertyHandle)
-    SLATE_END_ARGS()
+	SLATE_BEGIN_ARGS(SDeadlineCloudFloatWidget) {}
+		SLATE_ARGUMENT(TSharedPtr<IPropertyHandle>, PropertyHandle)
+	SLATE_END_ARGS()
 
-    void Construct(const FArguments& InArgs)
-    {
-        Property = InArgs._PropertyHandle;
+	void Construct(const FArguments& InArgs)
+	{
+		Property = InArgs._PropertyHandle;
 
-        ChildSlot
-            [
-                SNew(SHorizontalBox)
-                    + SHorizontalBox::Slot()
-                    .FillWidth(1.0f)
-                    .VAlign(VAlign_Center)
-                    [
-                        SNew(SNumericEntryBox<float>)
-                            .Font(IDetailLayoutBuilder::GetDetailFont())
-                            .AllowSpin(false)
-                            .MinDesiredValueWidth(50.0f)
-                            .Value_Lambda([this]
-                                {
-                                    FString String;
-                                    Property->GetValue(String);
+		ChildSlot
+			[
+				SNew(SHorizontalBox)
+					+ SHorizontalBox::Slot()
+					.FillWidth(1.0f)
+					.VAlign(VAlign_Center)
+					[
+						SNew(SNumericEntryBox<float>)
+							.Font(IDetailLayoutBuilder::GetDetailFont())
+							.AllowSpin(false)
+							.MinDesiredValueWidth(50.0f)
+							.Value_Lambda([this]
+								{
+									FString String;
+									Property->GetValue(String);
 
-                                    return FCString::Atof(*String);
-                                })
-                            .OnValueCommitted_Lambda([this](float Value, ETextCommit::Type)
-                                {
-                                    Property->SetValue(FString::SanitizeFloat(Value));
-                                })
-                    ]
-            ];
-    }
+									return FCString::Atof(*String);
+								})
+							.OnValueCommitted_Lambda([this](float Value, ETextCommit::Type)
+								{
+									Property->SetValue(FString::SanitizeFloat(Value));
+								})
+					]
+			];
+	}
 
 private:
 
-    TSharedPtr<IPropertyHandle> Property;
+	TSharedPtr<IPropertyHandle> Property;
 };
 
 void FDeadlineCloudDetailsWidgetsHelper::SConsistencyWidget::Construct(const FArguments& InArgs) {
 
-    OnFixButtonClicked = InArgs._OnFixButtonClicked;
+	OnFixButtonClicked = InArgs._OnFixButtonClicked;
 
-    ChildSlot
-        [
-            SNew(SHorizontalBox)
-                + SHorizontalBox::Slot()
-                .AutoWidth()
-                .Padding(5)
-                [
-                    SNew(STextBlock)
-                        .Text(FText::FromString("Parameters changed. Update parameters?"))
-                        .ColorAndOpacity(FLinearColor::Yellow) //
-                ]
+	ChildSlot
+		[
+			SNew(SHorizontalBox)
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.Padding(5)
+				[
+					SNew(STextBlock)
+						.Text(FText::FromString("Parameters changed. Update parameters?"))
+						.ColorAndOpacity(FLinearColor::Yellow) //
+				]
 
-                + SHorizontalBox::Slot()
-                .AutoWidth()
-                [
-                    SNew(SButton)
-                        .Text(FText::FromString("OK"))
-                        .OnClicked(this, &SConsistencyWidget::HandleButtonClicked)
-                ]
-        ];
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				[
+					SNew(SButton)
+						.Text(FText::FromString("OK"))
+						.OnClicked(this, &SConsistencyWidget::HandleButtonClicked)
+				]
+		];
 };
 
 void FDeadlineCloudDetailsWidgetsHelper::SEyeUpdateWidget::Construct(const FArguments& InArgs) {
 
-    OnEyeUpdateButtonClicked = InArgs._OnEyeUpdateButtonClicked;
-    bShowHidden = InArgs._bShowHidden_;
+	OnEyeUpdateButtonClicked = InArgs._OnEyeUpdateButtonClicked;
+	bShowHidden = InArgs._bShowHidden_;
 
-    ChildSlot
-        [
-            SNew(SHorizontalBox)
-                + SHorizontalBox::Slot()
-                .AutoWidth()
-                .Padding(5)
-                [
-                    SNew(STextBlock)
-                        .Text(FText::FromString("Some parameters will be hidden in MRQ. "))
-                ]
+	ChildSlot
+		[
+			SNew(SHorizontalBox)
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.Padding(5)
+				[
+					SNew(STextBlock)
+						.Text(FText::FromString("Some parameters will be hidden in MRQ. "))
+				]
 
-                + SHorizontalBox::Slot()
-                .AutoWidth()
-                .Padding(5)
-                [
-                    SNew(SButton)
-                        .OnClicked(this, &SEyeUpdateWidget::HandleButtonClicked)
-                        .Text(this, &SEyeUpdateWidget::GetButtonText)
-                ]
-        ];
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.Padding(5)
+				[
+					SNew(SButton)
+						.OnClicked(this, &SEyeUpdateWidget::HandleButtonClicked)
+						.Text(this, &SEyeUpdateWidget::GetButtonText)
+				]
+		];
 };
 
 
@@ -258,60 +262,60 @@ TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreatePropertyWidgetByTy
 	}
 	}
 
-    return SNullWidget::NullWidget;
+	return SNullWidget::NullWidget;
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateNameWidget(FString Parameter)
 {
-    return  SNew(SHorizontalBox)
-        + SHorizontalBox::Slot()
-        .Padding(FMargin(0.0f, 1.0f, 0.0f, 1.0f))
-        .FillWidth(1)
-        [
-            SNew(STextBlock)
-                .Text(FText::FromString(Parameter))
-                .Font(IDetailLayoutBuilder::GetDetailFont())
-                .ColorAndOpacity(FSlateColor::UseForeground())
-        ];
+	return  SNew(SHorizontalBox)
+		+ SHorizontalBox::Slot()
+		.Padding(FMargin(0.0f, 1.0f, 0.0f, 1.0f))
+		.FillWidth(1)
+		[
+			SNew(STextBlock)
+				.Text(FText::FromString(Parameter))
+				.Font(IDetailLayoutBuilder::GetDetailFont())
+				.ColorAndOpacity(FSlateColor::UseForeground())
+		];
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateConsistencyWidget(FString ResultString)
 {
-    TSharedRef<SConsistencyWidget> ConsistensyWidget = SNew(SConsistencyWidget)
-        .CheckResult(ResultString)
-        .Visibility(EVisibility::Collapsed);
-    return  ConsistensyWidget;
+	TSharedRef<SConsistencyWidget> ConsistensyWidget = SNew(SConsistencyWidget)
+		.CheckResult(ResultString)
+		.Visibility(EVisibility::Collapsed);
+	return  ConsistensyWidget;
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateEyeUpdateWidget()
 {
-    TSharedRef<SEyeUpdateWidget> EyeUpdateWidget = SNew(SEyeUpdateWidget)
-        .Visibility(EVisibility::Collapsed);
-    return  EyeUpdateWidget;
+	TSharedRef<SEyeUpdateWidget> EyeUpdateWidget = SNew(SEyeUpdateWidget)
+		.Visibility(EVisibility::Collapsed);
+	return  EyeUpdateWidget;
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreatePathWidget(TSharedPtr<IPropertyHandle> ParameterHandle)
 {
-    return SNew(SDeadlineCloudFilePathWidget)
-        .PathPropertyHandle(ParameterHandle);
+	return SNew(SDeadlineCloudFilePathWidget)
+		.PathPropertyHandle(ParameterHandle);
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateIntWidget(TSharedPtr<IPropertyHandle> ParameterHandle)
 {
-    return SNew(SDeadlineCloudIntWidget)
-        .PropertyHandle(ParameterHandle);
+	return SNew(SDeadlineCloudIntWidget)
+		.PropertyHandle(ParameterHandle);
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateFloatWidget(TSharedPtr<IPropertyHandle> ParameterHandle)
 {
-    return SNew(SDeadlineCloudFloatWidget)
-        .PropertyHandle(ParameterHandle);
+	return SNew(SDeadlineCloudFloatWidget)
+		.PropertyHandle(ParameterHandle);
 }
 
 TSharedRef<SWidget> FDeadlineCloudDetailsWidgetsHelper::CreateStringWidget(TSharedPtr<IPropertyHandle> ParameterHandle)
 {
-    return SNew(SDeadlineCloudStringWidget)
-        .StringPropertyHandle(ParameterHandle);
+	return SNew(SDeadlineCloudStringWidget)
+		.StringPropertyHandle(ParameterHandle);
 }
 
 
